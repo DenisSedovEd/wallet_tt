bffa2505564840272f4411486f2c991cd9e5f5d4
SDE <deanisik@gmail.com>
2025-07-04 11:50:49 +0700

    make migration

---
+++ wallet_app/alembic/versions/2025_07_04_0951-9146422e930d_create_wallet_table.py
@@ -0,0 +1,34 @@
+"""create wallet table
+
+Revision ID: 9146422e930d
+Revises:
+Create Date: 2025-07-04 09:51:20.161487
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "9146422e930d"
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table(
+        "wallets",
+        sa.Column("uuid", sa.UUID(), nullable=False),
+        sa.Column("balance", sa.Numeric(precision=20, scale=2), nullable=False),
+        sa.PrimaryKeyConstraint("uuid", name=op.f("pk_wallets")),
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_table("wallets")
